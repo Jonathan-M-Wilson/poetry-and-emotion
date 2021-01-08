0f423fc3047b19b2934e8b5e9d9a96f5ae56b22b
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-08 10:29:57 -0700

    Create poro with tone method

---
+++ app/poros/tone.rb
@@ -0,0 +1,13 @@
+class Tone
+  attr_reader :tones
+
+  def initialize(data)
+    @tones = data[:document_tone][:tones]
+  end
+
+  def tone
+    @tones.reduce([]) do |acc, tone|
+      acc << tone[:tone_name]
+    end
+  end
+end
