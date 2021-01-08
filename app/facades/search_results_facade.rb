6f2a1ddc7a9082868892617bc780ce2129053b24
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-08 10:29:27 -0700

    add get_tone method to search facade

--- app/facades/search_results_facade.rb
+++ app/facades/search_results_facade.rb
@@ -6,5 +6,10 @@ class SearchResultsFacade
         Poem.new(poem_data)
       end
     end
+
+    def get_tone(string)
+      data = IBMService.new.get_tone(string)
+      Tone.new(data)
+    end
   end
 end
