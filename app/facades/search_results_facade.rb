5a7c349e776622f832a7c28d396022049c0f0ac1
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-08 09:45:22 -0700

    rename poetry service

--- app/facades/search_results_facade.rb
+++ app/facades/search_results_facade.rb
@@ -1,7 +1,7 @@
 class SearchResultsFacade
   class << self
     def get_poems(author)
-      poems = PoetryDBService.get_poems(author)
+      poems = PoetryService.get_poems(author)
       poems.map do |poem_data|
         Poem.new(poem_data)
       end
